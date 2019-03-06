dd435c46b148318136079a8c00181f5880f0967b
sergey-lasckotko <anaharet@tut.by>
2019-03-06 13:05:58 +0300

    Закончил service-owner компонент

--- src/app/service-owner/service-owner.component.ts
+++ src/app/service-owner/service-owner.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service-owner.component.css']
 })
 export class ServiceOwnerComponent implements OnInit {
+  username: string = 'SERVICE Owner';
+  balance: string = '23750';
+  serviceProviders: any[] = [
+    'Service Provider 1',
+    'Service Provider 2',
+    'Service Provider 3',
+    'Service Provider 4',
+    'Service Provider 5',
+  ];
+  smartContracts: any[] = [
+    'Contract 1',
+    'Contract 1',
+    'Contract 1',
+    'Contract 1',
+    'Contract 1',
+    'Contract 1'
+  ];
+  chosen: string = "Никто не выбран";
+  isModalOpened: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  openModel(sp: any) {
+    this.chosen = sp;
+    this.isModalOpened = true;
+  }
+
+  closeModal() {
+    this.chosen = "Никто не выбран";
+    this.isModalOpened = false;
+  }
 }
